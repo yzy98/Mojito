fee02b6bd81fd96d5d662fffa22605f08e49d42b
yzy98 <yz186337@163.com>
2025-07-01 11:07:09 +0800

    add navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import gsap from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
+import Navbar from "./components/navbar";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
   return (
-    <div className="flex-center">
-      <h1 className="text-3xl bg-indigo-300">Hello</h1>
-    </div>
+    <main>
+      <Navbar />
+    </main>
   );
 };
 
